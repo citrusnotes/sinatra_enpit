0ef1050625226c149388d66b9310fac397e39579
vagrant <vagrant@enpit>
2015-09-23 05:29:43 +0000

    Update

--- hello.rb
+++ hello.rb
@@ -2,4 +2,14 @@ require 'sinatra'
 
 get '/' do
  "Hello World!"
+
+get '/foo', :agent => /Songbird (\d\.\d)[\d\/]*?/ do
+  "Songbirdのバージョン #{params['agent'][0]}を使ってます。"
+end
+
+get '/foo' do
+  # Songbird以外のブラウザにマッチ
+end
+
+
 end
